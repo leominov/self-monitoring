f25842944a60a0f4b57a9badff8d55046c837f5c
leoderoko <leoderoko@gmail.com>
2016-04-20 10:46:06 +0500

    Update installer

--- install-gomon.sh
+++ install-gomon.sh
@@ -9,33 +9,67 @@ GOMON_BINARY="gomon"
 
 export GOMON_WD
 
+log_info() {
+    echo "-----> $*"
+}
+
+log_verbose() {
+    echo "       $*"
+}
+
+log_warn() {
+    echo " !     $*"
+}
+
+log_fail() {
+    echo "$@" 1>&2
+    exit 1
+}
+
+log_info "Gomon Installer"
+
 if [ ! "$GOPATH" ]; then
-	echo >&2 "ERROR: Missing GOPATH; please see https://golang.org/doc/code.html#GOPATH"
-	exit 1
+    log_fail "ERROR: Missing GOPATH; please see https://golang.org/doc/code.html#GOPATH"
 fi
 
 if [[ $EUID -ne 0 ]]; then
-    echo >&2 "ERROR: Must be run with root privileges."
-    exit 1
+    log_fail "ERROR: Must be run with root privileges."
 fi
 
-if [ ! -f $GOMON_BINARY ]; then
-    make build
-fi
+log_info "Building Gomon"
+make build
+log_verbose "Done"
 
-if [ ! -d $GOMON_WD ]; then
+if [ ! -d "$GOMON_WD" ]; then
+    log_verbose "Making directory: $GOMON_WD"
     mkdir -p $GOMON_WD
+    log_verbose "Done"
 fi
 
-if [ -f $GOMON_INIT ]; then
+if [ -f "$GOMON_INIT" ]; then
+    log_info "Stopping Gomon"
     $GOMON_INIT stop
+    log_verbose "Done"
 else
+    log_info "Coping init script"
     cp $GOMON_INIT_SOURCE $GOMON_INIT
+    update-rc.d gomon defaults
+    log_verbose "Done"
 fi
 
+if [ -f "$GOMON_WD/$GOMON_CONFIG" ]; then
+    log_info "Coping config file"
+    cp $GOMON_CONFIG $GOMON_WD
+    log_verbose "Done"
+else
+    log_warn "Please do not forget to update your monitoring configuration and restart the monitoring:"
+    log_warn "$GOMON_INIT restart"
+fi
+
+log_info "Coping binary file"
 cp gomon $GOMON_WD
-cp $GOMON_CONFIG $GOMON_WD
+log_verbose "Done"
 
-update-rc.d gomon defaults
-
+log_info "Starting Gomon"
 $GOMON_INIT start
+log_verbose "Done"
