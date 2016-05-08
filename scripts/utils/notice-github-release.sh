92f447d91b325760eedbfe0e6807d5ab6e9e6a91
leoderoko <leoderoko@gmail.com>
2016-05-08 22:27:50 +0500

    Update notice

--- scripts/utils/notice-github-release.sh
+++ scripts/utils/notice-github-release.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 URL='https://api.telegram.org/bot'$TELEGRAM_API_TOKEN
-CHAT_ID=$TELEGRAM_CHAT_ID
 MSG_URL=$URL'/sendMessage'
 
 VERSION=$1
@@ -10,4 +9,4 @@ MSG_TEXT="🚀 Gomon v$VERSION released
 $RELEASE_URL"
 
 echo "Sending release notice"
-res=$(curl -s "$MSG_URL" -F "chat_id=$CHAT_ID" -F "text=$MSG_TEXT" -F "parse_mode=html")
+res=$(curl -s "$MSG_URL" -F "chat_id=$TELEGRAM_CHAT_ID" -F "text=$MSG_TEXT" -F "parse_mode=html")
