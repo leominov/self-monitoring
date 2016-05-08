6d037a8175e5c85c5714e838b88389b2c02ae460
leoderoko <leoderoko@gmail.com>
2016-05-08 23:05:53 +0500

    Add tinyurl.com for release link

--- scripts/utils/notice-github-release.sh
+++ scripts/utils/notice-github-release.sh
@@ -5,8 +5,8 @@ MSG_URL=$URL'/sendMessage'
 
 VERSION=$1
 RELEASE_URL=https://github.com/leominov/self-monitoring/releases/tag/$VERSION
-MSG_TEXT="🚀 Gomon v$VERSION released
-$RELEASE_URL"
 
 echo "Sending release notice"
+RELEASE_URL=$(curl -s http://tinyurl.com/api-create.php?url=$RELEASE_URL)
+MSG_TEXT="🚀 Gomon v$VERSION released $RELEASE_URL"
 res=$(curl -s "$MSG_URL" -F "chat_id=$TELEGRAM_CHAT_ID" -F "text=$MSG_TEXT" -F "parse_mode=html")
