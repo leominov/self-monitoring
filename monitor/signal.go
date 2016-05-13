4691d882d7992810da8bb3c8c47fb59bb2a459e1
leoderoko <leoderoko@gmail.com>
2016-05-13 12:47:47 +0500

    Add lost file

---
+++ monitor/signal.go
@@ -0,0 +1,30 @@
+package monitor
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/leominov/self-monitoring/msignal"
+)
+
+// SignalRoutine loop
+func (monitor *Monitor) SignalRoutine() {
+	for {
+		s := <-msignal.SignalChan
+		switch s {
+		case msignal.ReloadSignal:
+			logrus.Infoln("Reloading configuration...")
+			monitor.Configure()
+			logrus.Infoln("Done.")
+
+		case msignal.QuitSignal:
+			logrus.Infoln("Received shutdown signal")
+			msignal.ExitChan <- 0
+
+		case msignal.InfoSignal:
+			logrus.Infoln("Counter:", monitor.Counter)
+			logrus.Infoln("Service list:", monitor.ServiceList)
+
+		default:
+			logrus.Infoln("Catched unknown signal")
+		}
+	}
+}
